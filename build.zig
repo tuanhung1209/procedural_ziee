9032b23dd52939d761a9ed1cb46960033c2617ba
imsuchatcode <firekatana1209@gmail.com>
2026-09-01 17:08:25 +0700

    add c.h for compatiblity, translation_c in zigfile

--- build.zig
+++ build.zig
@@ -4,17 +4,25 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
+    const translate_c = b.addTranslateC(.{
+        .root_source_file = b.path("src/c.h"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    translate_c.linkSystemLibrary("glfw", .{});
+
     const exe = b.addExecutable(.{
         .name = "procedural_ziee",
         .root_module = b.createModule(.{
             .root_source_file = b.path("src/main.zig"),
+            .imports = &.{.{ .name = "c", .module = translate_c.createModule() }},
             .target = target,
             .optimize = optimize,
             .link_libc = true,
         }),
     });
 
-    exe.root_module.linkSystemLibrary("glfw", .{});
     exe.root_module.linkSystemLibrary("vulkan", .{});
 
     b.installArtifact(exe);
